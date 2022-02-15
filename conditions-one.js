0f0e69e4bc5a5aa7ffa476a50a8023678032131e
arun jacob <arunjacob991@gmail.com>
2022-02-15 21:54:37 +0530

    conditions

---
+++ conditions-one.js
@@ -0,0 +1,44 @@
+let yoyo = "hoyya";
+
+if (yoyo === "hoyya") {
+    console.log("hoyya");
+}else{
+    console.log("no hoyya");
+}
+
+
+let num = "one"
+switch (num){
+    case "one": console.log("one");
+    break;
+    case "two": console.log("two");
+    break;
+    case "three": console.log("three");
+    break;
+    default:
+        console.log("no cases");
+        break;
+
+}
+
+let number = "ten"
+switch(number){
+    case "ten": {
+        let message = "the first number";
+        console.log(message);
+        break;
+    }
+    case "twenty": {
+        let message = "the second number"
+        console.log(message);
+        break;
+    }
+    default:
+        console.log("there is no number");
+        break;
+}
+
+
+console.log("return");
+return;
+console.log("return ends above");
---
+++ conditions-two.js
@@ -0,0 +1,17 @@
+const value = 10;
+let item = 0;
+
+while(item < value){
+    item++;
+    console.log(`while is looping until value is less than item : ${item}`);
+}
+
+for(let i = 0; i < 5; i++ ){
+    console.log(i);
+}
+
+// for(;;){
+//     console.log("all high loops");
+// }
+
+// above is infinite loop using for...
